99089b11ab09be9d6b8683dc0e306aef47f8d781
Aman Kumar <96618245+Aman123x@users.noreply.github.com>
2023-12-11 10:43:53 +0000

    first

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,21 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const weather={ temperature: 25, conditions: "Sunny"};
+  const threshold=20;
+
+  const tempColor=weather.temperature>threshold?"red":"blue";
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <p style={{color:tempColor}}>
+            Temp:{weather.temperature}
+        </p>
+        <p>
+          Conditions:{weather.conditions}
+        </p>
     </div>
   )
 }
